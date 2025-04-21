8eadd6a04e24394355f5aeb6ce5f6b8ab4998fe5
jostoz <jostoztado@gmail.com>
2025-04-20 21:56:50 -0600

    Update package.json and refactor version check script
    - Changed npm version requirement in package.json to allow any version >=8.x.
    - Refactored check-versions.js to use ES module syntax and streamline version checking logic.
    - Improved warning messages for version mismatches to provide clearer guidance for users.

--- build/check-versions.js
+++ build/check-versions.js
@@ -1,54 +1,41 @@
-'use strict'
-const chalk = require('chalk')
-const semver = require('semver')
-const packageConfig = require('../package.json')
-const shell = require('shelljs')
+import chalk from 'chalk'
+import semver from 'semver'
+import { execSync } from 'node:child_process'
+import packageConfig from '../package.json' assert { type: 'json' }
 
-function exec (cmd) {
-  return require('child_process').execSync(cmd).toString().trim()
-}
+const exec = cmd => execSync(cmd).toString().trim()
 
 const versionRequirements = [
   {
     name: 'node',
-    currentVersion: semver.clean(process.version),
+    currentVersion: process.version,
     versionRequirement: packageConfig.engines.node
   }
 ]
 
-if (shell.which('npm')) {
-  versionRequirements.push({
-    name: 'npm',
-    currentVersion: exec('npm --version'),
-    versionRequirement: packageConfig.engines.npm
-  })
+const versionNpm = exec('npm --version')
+
+versionRequirements.push({
+  name: 'npm',
+  currentVersion: versionNpm,
+  versionRequirement: packageConfig.engines.npm
+})
+
+const warnings = []
+
+for (const mod of versionRequirements) {
+  if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
+    warnings.push(chalk.yellow(
+      `You are using ${mod.name} ${mod.currentVersion} but this version requires ${mod.name} ${mod.versionRequirement}. ${mod.name === 'node' ? 'Please upgrade your Node version.' : 'Consider using a compatible NPM version.'}`
+    ))
+  }
 }
 
-module.exports = function () {
-  const warnings = []
-
-  for (let i = 0; i < versionRequirements.length; i++) {
-    const mod = versionRequirements[i]
-
-    if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
-      warnings.push(mod.name + ': ' +
-        chalk.red(mod.currentVersion) + ' should be ' +
-        chalk.green(mod.versionRequirement)
-      )
-    }
+if (warnings.length) {
+  console.warn('\n****************************************\n')
+  for (const warning of warnings) {
+    console.warn(warning)
   }
-
-  if (warnings.length) {
-    console.log('')
-    console.log(chalk.yellow('To use this template, you must update following to modules:'))
-    console.log()
-
-    for (let i = 0; i < warnings.length; i++) {
-      const warning = warnings[i]
-      console.log('  ' + warning)
-    }
-
-    console.log()
-    process.exit(1)
-  }
+  console.warn('\n****************************************\n')
+  process.exit(1)
 }
